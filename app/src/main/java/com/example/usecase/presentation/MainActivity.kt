cffe6b4019079a462abb7b984a0f90eac394b4ba
tatyanaivanouskaya <tatyana.ivanouskaya@gmail.com>
2022-01-31 16:36:26 +0300

    MVVM

--- app/src/main/java/com/example/usecase/presentation/MainActivity.kt
+++ app/src/main/java/com/example/usecase/presentation/MainActivity.kt
@@ -5,39 +5,36 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.usecase.R
-import com.example.usecase.data.UserRepositoryImpl
-import com.example.usecase.data.storage.UserStorage
-import com.example.usecase.data.storage.sharedprefs.SharedPrefUserStorage
-import com.example.usecase.domain.models.SaveUserName
-import com.example.usecase.domain.usecase.GetUserNameUseCase
-import com.example.usecase.domain.usecase.SaveUserNameUseCase
 
 class MainActivity : AppCompatActivity() {
 
-    private val userStorage by lazy {SharedPrefUserStorage(context = applicationContext)}
-    private val userRepository by lazy { UserRepositoryImpl(userStorage = userStorage)}
-    private val getUserNameUseCase by lazy { GetUserNameUseCase(userRepository = userRepository)}
-    private val saveUserNameUseCase by lazy { SaveUserNameUseCase(userRepository = userRepository)}
+    private lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vm = ViewModelProvider(this, MainViewModelFactory(this))
+            .get(MainViewModel::class.java)
 
         val dataTextView = findViewById<TextView>(R.id.dataTextView)
         val dataEditView = findViewById<EditText>(R.id.dataEditText)
         val sendButton = findViewById<Button>(R.id.sendButton)
         val receiveButton = findViewById<Button>(R.id.receiveButton)
 
+        vm.resultData.observe(this, Observer {
+            dataTextView.text = it
+        })
+
         sendButton.setOnClickListener {
             val text = dataEditView.text.toString()
-            val saveUserName = SaveUserName(name = text)
-            val result = saveUserNameUseCase.execute(saveUserName)
-            dataTextView.text = "Save result = $result"
+            vm.save(text = text)
         }
         receiveButton.setOnClickListener {
-            val userName = getUserNameUseCase.execute()
-            dataTextView.text = "${userName.firstName} ${userName.lastName}"
+            vm.load()
         }
     }
 }
---
+++ app/src/main/java/com/example/usecase/presentation/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.usecase.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.usecase.domain.models.SaveUserName
+import com.example.usecase.domain.usecase.GetUserNameUseCase
+import com.example.usecase.domain.usecase.SaveUserNameUseCase
+
+class MainViewModel (
+    val getUserNameUseCase: GetUserNameUseCase, val saveUserNameUseCase: SaveUserNameUseCase
+        ): ViewModel(){
+
+    private val resultDataMutable = MutableLiveData<String>()
+    val resultData: LiveData<String> = resultDataMutable
+
+    fun save(text: String){
+        val saveUserName = SaveUserName(name = text)
+        val result = saveUserNameUseCase.execute(saveUserName)
+        resultDataMutable.value = "Save result = $result"
+
+    }
+
+    fun load(){
+        val userName = getUserNameUseCase.execute()
+        resultDataMutable.value = "${userName.firstName} ${userName.lastName}"
+
+    }
+}
