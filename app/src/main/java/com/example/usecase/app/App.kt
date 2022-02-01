509c44b577d0fb973d22d3273d51b4c3d22d3990
tatyanaivanouskaya <tatyana.ivanouskaya@gmail.com>
2022-02-01 16:48:29 +0300

    Koin

--- app/src/main/java/com/example/usecase/app/App.kt
+++ app/src/main/java/com/example/usecase/app/App.kt
@@ -10,11 +10,12 @@ import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class App: Application (){
+
     override fun onCreate() {
         super.onCreate()
 
         startKoin{
-            androidLogger(Level.DEBUG)
+            androidLogger(Level.ERROR)
             androidContext(this@App)
             modules(listOf(appModule, dataModule, domainModule))
         }
